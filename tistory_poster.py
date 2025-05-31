21253abaeb6b3363bff47bc535b36a57e271fcae
braveDevRyan <bluebears.ryan@gmail.com>
2025-05-31 19:07:32 +0900

    screenshot add

--- tistory_poster.py
+++ tistory_poster.py
@@ -62,35 +62,30 @@ def post_to_tistory(username, password, blog_name, title_text, content_text):
         # 4. 로그인 버튼 클릭
         login_button = driver.find_element(By.CLASS_NAME, "submit")
         login_button.click()
-        time.sleep(5)
-
-        write_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "a.link_tab"))
-        )
-        driver.save_screenshot('screenshot.png')
-
-        # # 4. 글관리 페이지로 이동
-        # post_list_url = f"https://{blog_name}.tistory.com/manage/"
-        # driver.get(post_list_url)
-        # time.sleep(2)
+        time.sleep(2)
 
 
+        # 5 글관리 페이지로 이동
+        post_list_url = f"https://{blog_name}.tistory.com/manage/post"
+        driver.get(post_list_url)
+        time.sleep(2)
 
-        # 6. 글쓰기 버튼 클릭
+
+        #6. 글쓰기 버튼 클릭
         # write_button = WebDriverWait(driver, 10).until(
         #     EC.element_to_be_clickable((By.CLASS_NAME, "link_write"))
         # )
-        write_button.click()
-        time.sleep(2)
+        # write_button.click()
+        # time.sleep(2)
 
         WebDriverWait(driver, 30).until(
             lambda driver: driver.execute_script('return document.readyState') == 'complete'
         )
-        driver.save_screenshot('screenshot.png')
 
         # 제목 입력 후, 첫 alert 처리 (임시저장 이어쓰기 여부)
         accept_if_alert_present("제목",driver, action="dismiss")  # 취소(dismiss)할 경우
         time.sleep(2)
+        driver.save_screenshot('screenshot.png')
 
         # 7. 제목 입력
         title_input = WebDriverWait(driver, 30).until(
